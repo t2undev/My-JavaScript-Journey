fda84064edf1483259c830c878561c6d4119771d
Babatunde <d2ndeayoade@gmail.com>
2024-06-07 02:40:39 +0100

    i started working on the ATM machine project logic(functionality)

--- projects/Simple_ATM_App/Simple_ATM_App.js
+++ projects/Simple_ATM_App/Simple_ATM_App.js
@@ -1,1 +1,29 @@
-alert('javascript is connected')
+// let alert this string to the browser to confirm that the javascript is connected to the html file
+// alert('javascript is connected')
+
+let accountBalance = 50000
+// we want to add this ballance paragraph element diplaying the balance so we we need to get the element through the dom 
+
+var displayBalance = document.getElementById('displayBalance')
+displayBalance.innerHTML += `<span>${accountBalance}</span>`
+
+
+
+var balButton = document.getElementById('balButton')
+
+balButton.addEventListener('click', function() {
+  console.log('hello world');
+
+})
+
+
+var depoButton = document.getElementById('depoButton')
+depoButton.addEventListener('click', function(){
+  var userInpu = document.getElementById('userInp').value
+  console.log(userInpu);
+  let newBalance = (accountBalance + userInpu)
+  console.log(newBalance);
+  displayBalance.innerHTML += `<span>${newBalance}</span>`
+  
+  // alert(userInpu + ' Has been successfully added to your balance. your new account ballance is ' + newBalance) 
+})
