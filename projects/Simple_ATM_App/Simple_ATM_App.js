8cc5fa1c105854a250fe75dbee33a1b2d6aaa587
Babatunde <d2ndeayoade@gmail.com>
2024-06-08 05:23:19 +0100

    I completed the withdrawer button functionality logic  on the ATM machine project.

--- projects/Simple_ATM_App/Simple_ATM_App.js
+++ projects/Simple_ATM_App/Simple_ATM_App.js
@@ -4,6 +4,11 @@
 let accountBalance = 50000
 // we want to add this ballance paragraph element diplaying the balance so we we need to get the element through the dom 
 
+var userName = prompt('Enter your name')
+
+document.getElementById('greeting').innerHTML = `Welcome back ${userName}`
+
+
 var span = document.getElementById('span')
 span.innerHTML = `${accountBalance}`
 
@@ -24,12 +29,44 @@ depoButton.addEventListener('click', function(){
 
   let depositAmount = Number(userInpu) //convert the input data to a number
   // console.log(typeof(depositAmount)); //return number value
-  accountBalance += depositAmount
+
+
+  if(!NaN && depositAmount > 0) {
+    accountBalance += depositAmount
   span.innerHTML = `${accountBalance}`
+  }else{
+    alert('Deposit Amount must be greater than zero')
+    
+  }
+  
+  if(!NaN && depositAmount > 0) {
+    alert(`${depositAmount} Has been successfully added to your balance. your account ballance is #${accountBalance}`)
+  }
+   
 
   userInpu = document.getElementById('userInp').value = ''
   userInpu = document.getElementById('userInp').focus()
 
   
-  // alert(userInpu + ' Has been successfully added to your balance. your new account ballance is ' + depositAmount) 
 })
+
+let withdrawBtn = document.getElementById('withdrawBtn')
+withdrawBtn.addEventListener('click', function(){
+  let debit = document.getElementById('userInp').value
+  // console.log(typeof(debit));
+  let debitAmount = Number(debit)
+  // console.log(typeof(debitAmount));
+  if (!NaN && debitAmount <= 0) {
+    alert('Debit amount must be above #0')
+  }else if (!NaN && debitAmount > accountBalance) {
+    alert('Insufficient fund, the debit amount is above your available ballance ')
+  }else if (!NaN && debitAmount < accountBalance) {
+    accountBalance -= debitAmount
+    span.innerHTML = `${accountBalance}`
+    alert(`${debitAmount} Has been debited from your account, your ballance is #${accountBalance}`)
+  }
+
+  userInpu = document.getElementById('userInp').value = ''
+  userInpu = document.getElementById('userInp').focus()
+})
+
