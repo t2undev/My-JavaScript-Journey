58d0944da3fe9fdadc80a742627e02e0b98b1103
Babatunde <d2ndeayoade@gmail.com>
2025-02-15 19:27:42 +0100

    made some changes to the interface and i also add a javascript code for the pause and stop buttons

--- projects/countdown_timer/script.js
+++ projects/countdown_timer/script.js
@@ -1,4 +1,8 @@
 let button = document.getElementById("btn");
+let timer; // timer variable will be used to store the setInterval function ID.
+let running = false; //this variable will be used to check if the timer is running or not.
+
+
 button.addEventListener("click", () => {
   let ss = document.getElementById("ss");
   let mm = document.getElementById("mm");
@@ -14,8 +18,15 @@ button.addEventListener("click", () => {
   minutes = parseInt(mm.value) || 0;
   hours = parseInt(hrs.value) || 0;
   days = parseInt(day.value) || 0;
+  // console.log(typeof seconds); 
 
-  let timer = setInterval(() => {
+  //if running is true, return immediately and do nothing.
+  if (running) return; 
+  
+  //if running is false, set it to true and continue with the code below.
+  running = true; 
+
+  timer = setInterval(() => {
     if (seconds > 0) {
       seconds -= 1;
       ss.value = seconds;
@@ -36,13 +47,51 @@ button.addEventListener("click", () => {
       seconds = 60;
     }else {
       clearInterval(timer);
+      running = false; //set running to false to allow the timer to start again.
       alert("Time is up!");
+      button.innerText = "Start"
     }
   
-  }, 1);
+  }, 1000);
 
-  stop
+  button.textContent = 'Running'; //change the button text to running when the timer starts.
+  paused.textContent = 'Pause'; //change the button text back from paused to pause when the timer starts.
 });
+
+
+
+let paused = document.getElementById('pausebtn');
+let stop = document.getElementById('stopbtn');
+
+
+
+paused.addEventListener('click', () => {
+  // alert('The pause button has been clicked');
+  clearInterval(timer);
+  running = false; //set running to false to allow the timer to start again.
+  button.textContent = 'Resume';
+  paused.textContent = 'Paused';
+
+});
+
+
+stop.addEventListener('click', () => {
+  clearInterval(timer); //clear the timer after the stop button has been clicked.
+  running = false; //set running to false to allow the timer to start again.
+
+  let ss = document.getElementById("ss");
+  let mm = document.getElementById("mm");
+  let hrs = document.getElementById("hrs");
+  let day = document.getElementById("day");
+
+  // reset all the input fields back to 0
+  ss.value = 0;
+  mm.value = 0;
+  hrs.value = 0;
+  day.value = 0;
+
+  button.textContent = 'Start'; //change the button text back to start when the timer stops.
+})
 
 
 
